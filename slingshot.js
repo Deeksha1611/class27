36426b703fc00fef40ddb97ca32f48fce8dbd0a2
deeksha1611 <deekshads2006@gmail.com>
2020-12-30 08:47:52 +0530

    created new class

---
+++ slingshot.js
@@ -0,0 +1,20 @@
+class SLINGSHOT {
+    constructor(a,b){
+            var options = {
+                bodyA:a,
+                bodyB:b,
+                stiffness: 0.04,
+                length: 10
+        }
+        this.body=Constraint.create(options);
+        World.add(world,this.body);
+    }
+    display(){
+        var posA=this.body.bodyA.position;
+        var posB=this.body.bodyB.position;
+        push();
+        strokeWeight(4);
+        line(posA.x,posA.y,posB.x,posB.y);
+        pop();
+    }
+}
